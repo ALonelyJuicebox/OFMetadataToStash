3d4e51f87adcce16bed621219311f439b8977f7e
JuiceBox <76471868+ALonelyJuicebox@users.noreply.github.com>
2021-12-22 02:09:41 -0800

    v1 release

---
+++ OFMetadataToStash.ps1
@@ -0,0 +1,138 @@
+<#
+---OnlyFans Metadata to Stash Database PoSH Script---
+
+AUTHOR
+    JuiceBox
+URL 
+    https://github.com/ALonelyJuicebox/OFMetadataToStash
+
+DESCRIPTION
+    Using the metadata database from DC's script, imports metadata such as the URL, post associated text, and creation date into your stash DB
+
+REQUIREMENTS
+    - Metadata database must be from DC's script
+    - The Powershell module "PSSQLite" must be installed https://github.com/RamblingCookieMonster/PSSQLite
+       Download a zip of the PSSQlite folder in that repo, extract it, run an Admin window of Powershell
+       in that directory then run 'install-module pssqlite' followed by the command 'import-module pssqlite'
+    - Change the paths to both your stash db and to the OnlyFans metadata db on the first two lines of this script
+ #>
+
+# USER DEFINED VARIABLES #
+##########################
+$PathToStashDatabase = "" #Please define the full file path. This file is probably in <filepath>/Stash/db/stash-go.sqlite
+$pathToOF_DB_Metadata = "" #Please define the full file path. This file should be located in <filepath>/<performername>/metadata/user_data.db
+##########################
+
+Clear-Host
+write-host "- OnlyFans Metadata to Stash Database PoSH Script - `n(https://github.com/ALonelyJuicebox/OFMetadataToStash)`n"
+if (!(test-path $PathToStashDatabase)){
+    read-host "You have not defined a filepath for Stash's database file. Please edit the first two lines of this .ps1 file.`nPress [Enter] to exit."
+    exit
+}
+elseif (!(test-path $pathToOF_DB_Metadata)){
+    read-host "You have not defined a filepath for the OnlyFans Metadata database. Please edit the first two lines of this .ps1 file.`nPress [Enter] to exit."
+    exit
+}
+else {
+    write-host "- Path to Stash's db: $PathToStashDatabase`n- Path to OnlyFans Metadata DB: $PathToOF_DB_Metadata`n"
+    
+    #Since we're editing the stash database directly, playing it safe and asking the user to back up their database
+    $backupConfirmation = Read-Host "Would you like to make a backup of your Stash Database? [Y/N] Default is 'Y'"
+    if ($backupConfirmation.substring(0,1) -ne 'n') {
+        $PathToStashDatabase = "C:\Users\Chris\Box\Active Scripts\OFMetadataToStash\stash\stash-go.sqlite" 
+        $PathToStashDatabaseBackup = Split-Path $PathToStashDatabase
+        $PathToStashDatabaseBackup = $PathToStashDatabaseBackup+"\stash-go_OFToStash_BACKUP.sqlite"
+        read-host "OK, A backup will be created at $PathToStashDatabaseBackup`nPress [Enter] to generate backup"
+
+        try {
+            Copy-Item $PathToStashDatabase -Destination $PathToStashDatabaseBackup
+        }
+        catch {
+            read-host "Unable to make a backup! Permissions error? Press [Enter] to exit"
+            exit
+        }
+        write-host "...Done! A backup was successfully created."
+    }
+    else{
+        write-host "OK, no backup will be created." 
+    }
+
+
+
+    read-host "Please make sure Stash is not running. `nPress [Enter] to begin the import"
+
+
+    #Let's capture the name of this performer based off of the file directory
+    $performername = Split-Path -parent $pathToOF_DB_Metadata
+    $performername = Split-Path -parent $performername 
+    $performername = Split-path $performername -leaf
+
+    #Select all the videos and the text the performer associated to them, if available
+    $Query = "SELECT posts.post_id AS posts_postID, posts.text, posts.created_at, medias.post_id AS medias_postID, medias.directory, medias.filename FROM medias INNER JOIN POSTS ON medias.post_id=posts.post_id WHERE medias.media_type ='Videos'"
+    $OFDBQueryResult = Invoke-SqliteQuery -Query $Query -DataSource $pathToOF_DB_Metadata
+
+    $numModified = 0
+    $numUnmodified = 0
+
+    foreach ($video in $OFDBQueryResult){
+
+        #Generating the URL for this post
+        $linktoperformerpage = "www.onlyfans.com/"+$video.posts_postID+"/"+$performername
+        
+        #Reformatting the date to something stash appropriate
+        $creationdatefromOF = $video.created_at
+        $creationdatefromOF = Get-Date $creationdatefromOF -format "yyyy-MM-dd"
+
+        #This defines the full filepath of the video. These are separate columns in the OFDB so we must combine them in a separate variable.
+        $OFDBfilename = $video.directory + "\" + $video.filename
+
+        #Creating the title we want for the scene
+        $title = "$performername - $creationdatefromOF"
+
+        #Looking in the stash DB for the current filename we're parsing, with an additional condition that the details must be empty
+        $Query = "SELECT * FROM SCENES WHERE path='"+$OFDBfilename+"'"
+        $StashDBQueryResult = Invoke-SqliteQuery -Query $Query -DataSource $PathToStashDatabase 
+
+        #Exception handling
+        if (!$StashDBQueryResult){
+            #If the scene is in the OnlyFans DB but not in stash, let the user know
+            if (Test-Path $OFDBfilename){
+                write-host "`n### INFO ###`nThere's a file in this OnlyFans metadata db that we couldn't find the following file in your Stash.`nThat said, this file *is* on your filesystem.`nTry running a Scan task in Stash and then run this script again.`n`n - $OFDBfilename`n" -ForegroundColor Cyan
+
+            }
+            #If the scene is in the OnlyFans DB but not on the filesystem, it might have been moved by the user. Prompt user to perform another OF scan
+            else{
+                write-host "`n### INFO ###`nThere's a file in this OnlyFans metadata db that we couldn't find the following file in your Stash.`nThis file also doesn't appear to be where the OnlyFans Metadata db thinks it should be.`nTry repopulating the OnlyFans metadata DB.`n`n - $OFDBfilename`n" -ForegroundColor Cyan
+            }
+        }
+        #If the video in question doesn't actually have any details from the OF DB
+        elseif($video.details -eq ""){
+            if ($StashDBQueryResult.title -ne $title){
+            
+                $modtime = get-date -format yyyy-MM-ddTHH:mm:ssK #Determining what the update_at time should be
+                
+                $Query = "UPDATE scenes SET title='"+$title+"', date='"+$creationdatefromOF+"', updated_at='"+$modtime+"', url='"+$linktoperformerpage+"' WHERE path='"+$OFDBfilename+"'"
+                $StashDBQueryResult = Invoke-SqliteQuery -Query $Query -DataSource $PathToStashDatabase 
+                write-host "Added metadata to Stash's database for the following file: `n- $OFDBfilename"   
+                $numModified++
+            }
+        }
+        #Otherwise let's check to see if this video needs updating in the stash DB, and if so, update accordingly
+        else {
+            if ($StashDBQueryResult.title -ne $title){
+                
+                $modtime = get-date -format yyyy-MM-ddTHH:mm:ssK #Determining what the update_at time should be
+
+                $Query = "UPDATE scenes SET title='"+$title+"', details='"+$video.text+"', date='"+$creationdatefromOF+"', updated_at='"+$modtime+"', url='"+$linktoperformerpage+"' WHERE path='"+$OFDBfilename+"'"
+                $StashDBQueryResult = Invoke-SqliteQuery -Query $Query -DataSource $PathToStashDatabase 
+                write-host "Added metadata to Stash's database for the following file: `n - $OFDBfilename" 
+                $numModified++  
+            }
+            else {
+                write-host "This file already has metadata, moving on...`n - $OFDBfilename"
+                $numUnmodified++
+            }
+        }    
+    }
+    write-host "`n****** Import Complete ******`n- Modified scenes: $nummodified`n- Scenes that already had metadata: $numunmodified"
+}
